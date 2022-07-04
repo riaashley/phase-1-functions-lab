f72777ae3c8de4586c51e7f039b85889395b84c8
Ria Legaspi <riaveluz@gmail.com>
2022-07-03 17:08:51 -0700

    commit tree 3736f93c1a0c62284c7267a5ac6d408554633378

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(x) {
+    let distance;
+        if (x > 42) {
+            distance = x - 42
+        }
+        else if (x <= 42) {
+            distance = 42 - x
+        }
+        return distance;
+}
+
+function distanceFromHqInFeet(x) {
+    let blocks = distanceFromHqInBlocks(x);
+    return blocks * 264;
+}
+function distanceTravelledInFeet(x, y){
+    let travelled;
+    if (x >= y) {
+        travelled = (x - y) * 264;
+    }
+    else if (x < y){
+        travelled = (y - x) * 264
+    }
+    return travelled;
+}
+
+function calculatesFarePrice(x, y) {
+    let fare;
+ let calc = distanceTravelledInFeet(x, y);
+    if (calc <= 400) {
+        fare = 0
+    }
+    else if ((calc >401) && (calc <=2000)) {
+        fare = (calc - 400) * .02;
+    }
+    else if (calc > 2001 && calc <= 2500) {
+        fare = 25
+    }
+    else if (calc > 2500) {
+        fare = 'cannot travel that far';
+    }
+    return fare;
+}
+  
+  
